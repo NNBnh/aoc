028b8af75356e8dcdfd1f6d471badf4ff7ca6e60
NNB <n.at@aleeas.com>
2023-01-22 11:30:19 +0700

    Update 2_1.rb

--- 2022/ruby/2_1.rb
+++ 2022/ruby/2_1.rb
@@ -1,8 +1,7 @@
 input
 .lines.map(&:split)
 .map do
-  1 + 'XYZ'.index(_2) + [3, 6, 0, 6, 0][
-    'XYZ'.index(_2) - 'ABC'.index(_1)
-  ]
+  1 + 'XYZ'.index(_2) +
+  [3, 6, 0]['XYZ'.index(_2) - 'ABC'.index(_1)]
 end
 .sum
