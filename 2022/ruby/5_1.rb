299b1917906d0a40f724932e63503d84281d1615
NNB <n.at@aleeas.com>
2023-01-23 12:15:43 +0700

    Update 5_1.rb

--- 2022/ruby/5_1.rb
+++ 2022/ruby/5_1.rb
@@ -4,7 +4,7 @@ File.read('input.txt')
 stacks.lines.map { _1[1..].chars.each_slice(4).map(&:first) }
 .transpose.map { _1.reverse.join.strip.chars } => stacks
 
-procedure.lines.map { _1.split(/[^\d]+/)[1..].map(&:to_i) }
+procedure.lines.map { _1.split(/\D+/)[1..].map(&:to_i) }
 .each { stacks[_3 - 1].append *stacks[_2 - 1].pop(_1).reverse }
 
 stacks.map(&:last).join
